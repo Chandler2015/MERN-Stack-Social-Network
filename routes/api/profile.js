49cf7ba637486bdcc9784b12bff091ffb328cbce
YuhangXiong <yxiong4@ualberta.ca>
2019-10-09 18:09:40 -0600

    add mongodb and routes

---
+++ routes/api/profile.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+//GET api/profile
+//test route
+//public
+router.get('/', (req, res) => res.send('Profile route'));
+
+module.exports = router;
---
+++ routes/api/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+//GET api/users
+//test route
+//public
+router.get('/', (req, res) => res.send('User route'));
+
+module.exports = router;
