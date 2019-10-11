40dd2dd49220aa203f12cef63682094316511dee
YuhangXiong <yxiong4@ualberta.ca>
2019-10-10 20:29:19 -0600

    finish backends

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
@@ -300,8 +302,8 @@ router.put(
   }
 );
 
-//DELETE api/profile/education:exp_id
-//delete profile experience
+//DELETE api/profile/education:edu_id
+//delete profile education
 //private
 router.delete('/education/:edu_id', auth, async (req, res) => {
   try {
@@ -323,4 +325,34 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   }
 });
 
+//DELETE api/profile/github/:username
+//get user repos from github
+//public
+
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret= ${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No github profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 module.exports = router;
